db4ec76486b06cb6e85f57984ab0dd93370608e7
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-02-02 16:23:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/anumitultrabotsbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/HelloUltra44/anjali /anjali
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
 fi
-cd /Eva
+cd /anjali
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
